257a05e356699ba9e4ab59f7154feaaf52003f6d
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2020-02-05 15:06:49 +0900

    add Makefile targets

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(name='{{cookiecutter.project_name}}',
       version="0.0.1",
-      packages=find_packages(),
+      packages=find_packages(exclude=["tests", "tests.*"]),
       install_requires=[
       ],
       entry_points={'console_scripts': ['{{cookiecutter.package_name}}={{cookiecutter.package_name}}:main']},
